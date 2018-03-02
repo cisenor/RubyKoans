937b7c02c30512338e0757873f70c2507b6a4c39
Craig <craig@stembolt.com>
2018-03-02 11:56:18 -0800

    Finished triangles and true/false

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,24 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # Check input
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError.new("A triangle side cannot be of length 0")
+  end
+  if a < 0 || b < 0 || c < 0
+    raise TriangleError.new("A triangle side's length cannot be negative")
+  end
+  if a + b <= c || b + c <= a || a + c <= b
+    raise TriangleError.new("The sum of any two sides of a triangle must be larger than the third side.")
+  end
+
+  if a == b && b == c
+    return :equilateral
+  elsif a == b || b == c || c == a
+    return :isosceles
+  else
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
